3ee565f17a3f6e54f8fe9f0375981af7ed6d5fba
Prateek Gupta <77004722+prateek9682@users.noreply.github.com>
2021-06-05 16:39:07 +0530

    Update Footer.js

--- Footer.js
+++ Footer.js
@@ -1,7 +1,10 @@
+import React from "react";
+
 function Footer() {
+  var currYear = new Date().getFullYear();
   return (
     <footer>
-      <p>Copyright 2021</p>
+      <p>Copyright @{currYear}</p>
     </footer>
   );
 }
