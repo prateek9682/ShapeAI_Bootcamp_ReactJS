1cb3bf1c34eabebbaba1f260e1ceb4ca207b15e6
Prateek Gupta <77004722+prateek9682@users.noreply.github.com>
2021-06-05 16:45:32 +0530

    Update Note.js

--- Note.js
+++ Note.js
@@ -9,22 +9,6 @@ export default function Note() {
           community of individual developers and companies.
         </p>
       </div>
-      <div className="note">
-        <h1>Components in React</h1>
-        <p>
-          Components are the building blocks of any React app and a typical
-          React app will have many of these. Simply put, a component is a
-          JavaScript class or function that optionally accepts inputs.
-        </p>
-      </div>
-
-      <div className="note">
-        <h1>JSX</h1>
-        <p>
-          JSX is an extension of the JavaScript language based on ES6, and is
-          translated into regular JavaScript at runtime.
-        </p>
-      </div>
     </>
   );
 }
